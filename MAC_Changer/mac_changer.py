d181e3ecbdfb8bcc21b6df385abde125446f8210
XoanOuteiro <xoanouteirojorge@gmail.com>
2024-01-11 18:05:43 -0500

    -+ refactoring MAC_Changer

--- MAC_Changer/mac_changer.py
+++ MAC_Changer/mac_changer.py
@@ -3,28 +3,25 @@
 import subprocess
 import optparse
 
-parser = optparse.OptionParser()
+
+def change_mac(interface : str, new_mac : str):
+    print(f"[+] Changing MAC address for interface: {interface} to {new_mac}")
+
+    subprocess.call(["ifconfig", interface, "down"])  # list calling instead of concatenations to avoid ; hijacking
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
 
 '''
 
     ---Definition of parser options
 
 '''
+parser = optparse.OptionParser()
+
 parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
 parser.add_option("-m", "--mac", dest="newMac", help="New MAC address")
 
 (options, arguments) = parser.parse_args()
 
-interface : str = options.interface
-newMac : str = options.newMac
-
-'''
-
-    ---Program logic
-
-'''
-print(f"[+] Changing MAC address for interface: {interface} to {newMac}")
-
-subprocess.call(["ifconfig", interface, "down"])  # list calling instead of concatenations to avoid ; hijacking
-subprocess.call(["ifconfig", interface, "hw", "ether", newMac])
-subprocess.call(["ifconfig", interface, "up"])
+change_mac(options.interface, options.newMac)
